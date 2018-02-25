67855346997e9df59c7fe1f1fa3a6da27255716d
chunky2808 <parasjain2896@gmail.com>
2018-02-25 18:50:43 +0530

    Site Working

--- Enlight/views.py
+++ Enlight/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Forum
+from .models import Forum,Topic
 
 def forum_list(request):
 	forums = Forum.objects.all()
@@ -7,3 +7,10 @@ def forum_list(request):
 	# 	print(fori.name)
 	# 	print(fori.desc)
 	return render(request,'home.html',{'forums':forums})
+
+def topic_list(request, pk):
+	topics = Topic.objects.all()
+	#topics = Topic.objects.get(pk=pk)
+	print(topics)
+	return render(request,'topics.html',{'topics' : topics})
+
--- src/urls.py
+++ src/urls.py
@@ -20,5 +20,6 @@ from Enlight import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^topics/(?P<pk>\d+)/$', views.topic_list, name='topic'),
     url(r'^$',views.forum_list,name='forum')
 ]
